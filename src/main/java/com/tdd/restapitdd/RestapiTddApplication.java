646769ebcda093d3f633e213019bb34295601e2e
Prabhash <kumarprabhash1995@gmail.com>
2021-08-16 19:14:37 +0530

    intial commit

---
+++ src/main/java/com/tdd/restapitdd/RestapiTddApplication.java
@@ -0,0 +1,13 @@
+package com.tdd.restapitdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestapiTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestapiTddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tdd/restapitdd/RestapiTddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tdd.restapitdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestapiTddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
