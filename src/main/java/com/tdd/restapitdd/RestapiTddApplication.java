09a3af4f38895f2eb67be60ccd32a820f698d648
Prabhash <kumarprabhash1995@gmail.com>
2021-08-17 00:33:23 +0530

    updated code with tdd practice

--- src/main/java/com/tdd/restapitdd/RestapiTddApplication.java
+++ src/main/java/com/tdd/restapitdd/RestapiTddApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestapiTddApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RestapiTddApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RestapiTddApplication.class, args);
+    }
 
 }
